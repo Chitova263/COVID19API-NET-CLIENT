3be02f64e8a52c5e7aef9c4c1dc4a1e651777f0b
Nigel Mukandi <mapiye.nigel@gmail.com>
2021-04-20 00:57:12 +0200

    refactor WebClient

--- Covid19.Client/WebClient/WebClient.cs
+++ Covid19.Client/WebClient/WebClient.cs
@@ -1,82 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
+﻿using FluentResults;
+using System;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
-using Covid19.Client.Models;
 
 namespace Covid19.Client
 {
     internal sealed class WebClient : IWebClient
     {
-
         private readonly HttpClient _httpClient;
 
-        public WebClient()
+        public WebClient() => _httpClient = new HttpClient();
+
+        /// <summary>
+        ///     Downloads data asynchronously from an URL and returns it
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<Result<string>> DownloadAsync(string uri, CancellationToken cancellationToken)
         {
-            _httpClient = new HttpClient();
-        }
+            if (string.IsNullOrWhiteSpace(uri))
+                throw new ArgumentException($"'{nameof(uri)}' cannot be null or whitespace", nameof(uri));
 
-        public async Task<(ResponseInfo, string)> DownloadAsync(string url, CancellationToken cancellationToken = default)
-        {
-            Validators.EnsureUrlIsValid(url);
+            HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, uri);
+            HttpResponseMessage res = await _httpClient
+                    .SendAsync(req, HttpCompletionOption.ResponseHeadersRead, cancellationToken)
+                    .ConfigureAwait(false);
 
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+            if (!res.IsSuccessStatusCode)
+                return Result.Fail($"Error: Network error connection failed");
 
-            HttpResponseMessage response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken)
-                .ConfigureAwait(false);
-
-            String content = await response.Content.ReadAsStringAsync()
-                .ConfigureAwait(false);
-
-            return (
-                new ResponseInfo
-                {
-                    StatusCode = response.StatusCode,
-                    Reason = response.ReasonPhrase,
-                    HeaderCollection = ConvertHeaders(response.Headers)
-                },
-                content
-            );
-        }
-
-        public async Task<(ResponseInfo, Stream)> DownloadRawAsync(string url, CancellationToken cancellationToken = default)
-        {
-            Validators.EnsureUrlIsValid(url);
-
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
-
-            HttpResponseMessage response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken)
-                .ConfigureAwait(false);
-
-            Stream contentStream = await response.Content.ReadAsStreamAsync()
-                .ConfigureAwait(false);
-
-            return(
-                new ResponseInfo
-                {
-                    HeaderCollection = ConvertHeaders(response.Headers),
-                    StatusCode = response.StatusCode,
-                    Reason = response.ReasonPhrase,
-                },
-                contentStream
-            );
-        }
-
-        private static WebHeaderCollection ConvertHeaders(HttpResponseHeaders headers)
-        {
-            WebHeaderCollection newHeaders = new WebHeaderCollection();
-            foreach (KeyValuePair<string, IEnumerable<string>> headerPair in headers)
-            {
-                foreach (string headerValue in headerPair.Value)
-                {
-                    newHeaders.Add(headerPair.Key, headerValue);
-                }
-            }
-            return newHeaders;
+            var content = await res.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+            return Result.Ok(content);
         }
 
         public void Dispose()
